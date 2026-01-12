e636a0019b184a1a0f0dcbec7708a7e2559d0313
kartikd2003 <165130007+kartikd2003@users.noreply.github.com>
2026-01-12 21:13:34 +0530

    Add User and Task models with SQLAlchemy

---
+++ models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True)
+    password = Column(String)
+
+class Task(Base):
+    __tablename__ = "tasks"
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"))
